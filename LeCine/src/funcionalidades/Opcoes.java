6b607234521763b0ca554760b993a02786d30385
HaagHenrique <henriquehaag11@gmail.com>
2022-09-16 21:50:04 -0300

    Commit

--- LeCine/src/funcionalidades/Opcoes.java
+++ LeCine/src/funcionalidades/Opcoes.java
@@ -1,5 +1,11 @@
 package funcionalidades;
 
-public class Opcoes {
+public class Opcoes extends Menu{
+    @Override
+    public void menu() {
+        super.menu();
+//        System.out.println("Digite o valor da opção desejada: [número inteiro]");
+    }
+
 
 }

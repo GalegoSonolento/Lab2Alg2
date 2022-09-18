fa1ab1173d97abf03185f8d33339cb75c055d33f
Guilherme Kaue Rockenach <111616845+Alemao-Chucrute@users.noreply.github.com>
2022-09-18 12:25:11 -0300

    Add files via upload

---
+++ Opcoes.java
@@ -0,0 +1,11 @@
+package funcionalidades;
+
+public class Opcoes extends Menu{
+    @Override
+    public void menu() {
+        super.menu();
+//        System.out.println("Digite o valor da opção desejada: [número inteiro]");
+    }
+
+
+}
